184dd783fcfa456cf0ff19213d09f9d2c64d695f
root <root@DAW2Jordi.b0yja5r41ubuli0gz2zlz4yjfe.ax.internal.cloudapp.net>
2019-03-22 16:22:16 +0000

    commit 2(2)

---
+++ formPeuPag.php
@@ -0,0 +1,11 @@
+<html>
+ <head>
+  <meta content ="text/html; charset=UTF-8" http-equiv="content-type">
+  <title>EXEMPLE DE FORMULARIS</title>
+ </head>
+ <body>
+  <h1>Creació d'un preu de pàgina per defecte</h1>
+  <form action="" method="POST">
+   Text del peu de pàgina: <input type="text" name="peupag"><br>
+ </form>
+ </body>
