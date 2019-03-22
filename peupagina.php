dc6e1407f69573f1348169a8dd2baa513f51c8bc
root <root@DAW2Jordi.b0yja5r41ubuli0gz2zlz4yjfe.ax.internal.cloudapp.net>
2019-03-22 15:06:15 +0000

    commit 1

---
+++ peupagina.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Peu de pàgina per defecte per a noves entrades
+Plugin URI: Escriu l'adreça URL completa del teu plugin dins de Github
+Description: Crea peus de pàgina per defecte per a noves entrades
+Version: 0.1
+Author: Jordi Velasco
+Author URI: https://github.com/JordiVelasco
+License: GNU GPLv3
+*/
+?>
